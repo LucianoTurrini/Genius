f2e0ded42fd5381f1cae76be4de2b8b525f0d19e
Luciano Turrini <luciano_turrini@hotmail.com>
2015-02-13 10:50:07 -0200

    Initial Commit

---
+++ Genius/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Genius
+//
+//  Created by Luciano Moreira Turrini on 2/13/15.
+//  Copyright (c) 2015 Luciano Moreira Turrini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
