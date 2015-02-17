b3d427bc2623f1b9d04d8f690259212312d5a79b
Luciano Turrini <luciano_turrini@hotmail.com>
2015-02-17 19:33:18 -0200

    while-menu-clear

--- Genius/main.m
+++ Genius/main.m
@@ -8,36 +8,50 @@
 
 #import <Foundation/Foundation.h>
 
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         int opcao;
-    
+        
+        do {
         //Exibe menu
         printf("\n 1)Cadastrar usuario \n\n 2)Jogar \n\n 0)Sair do Jogo \n\n Entre com o número opcão desejada: " );
-        
-        //Pegar Opção do Usuario e mandar para o Switch-Case
-        scanf ("%i", &opcao);
+
+            //Pegar Opção do Usuario e mandar para o Switch-Case
+            scanf ("%i", &opcao);
         
         switch (opcao)
         {
             case 0:
-                //implement
+                //Como o a funcao system("clear") não funciona para limpar o Terminal no Xcode, pulamos linhas para fazer uma simulação
+                for(int i= 0; i < 100; i++)
+                    printf("\n");
                 break;
             case 1:
-                //implement
+                //Como o a funcao system("clear") não funciona para limpar o Terminal no Xcode, pulamos linhas para fazer uma simulação
+                for(int i= 0; i < 100; i++)
+                    printf("\n");
                 break;
             case 2:
-                //implement
+                //Como o a funcao system("clear") não funciona para limpar o Terminal no Xcode, pulamos linhas para fazer uma simulação
+                for(int i= 0; i < 100; i++)
+                    printf("\n");
                 break;
             case 3:
-                //implement
+                //Como o a funcao system("clear") não funciona para limpar o Terminal no Xcode, pulamos linhas para fazer uma simulação
+                for(int i= 0; i < 100; i++)
+                    printf("\n");
                 break;
             default:
-                printf("\n\nA opção não existe!\n\n");
+                //Como o a funcao system("clear") não funciona para limpar o Terminal no Xcode, pulamos linhas para fazer uma simulação
+                for(int i= 0; i < 100; i++)
+                    printf("\n");
+                printf("\n\nA opção não existe! Tente Novamente\n\n");
                 break;
         }
-       
+        } while (opcao != 0);
+        
     }
     return 0;
 }
--- Jogador.h
+++ Jogador.h
@@ -8,6 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
+
+
 @interface Jogador : NSObject
 {
     NSString *nome;
