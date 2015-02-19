f57222113194b64685443da74debac4b9c0d0b8f
Jorge Henrique P. Garcia <j.hrique@hotmail.com>
2015-02-18 22:34:28 -0200

    Ordenação no ranking funcionando

--- Genius.m
+++ Genius.m
@@ -174,13 +174,32 @@
     int n = [jogadores count];
     
     
-    //Ordenação aqui
+    /* Teste ---
+    Jogador *jogador = [[Jogador alloc] initWithPont:@0 qtdJogadas:@0 pontAtual:@0 nome:@"User1"];
+        [jogador setMelhorPont: @20]; [self addJogador:jogador];
+    Jogador *jogador2 = [[Jogador alloc] initWithPont:@0 qtdJogadas:@0 pontAtual:@0 nome:@"User2"];
+        [jogador2 setMelhorPont: @243]; [self addJogador:jogador2];
+    Jogador *jogador3 = [[Jogador alloc] initWithPont:@0 qtdJogadas:@0 pontAtual:@0 nome:@"User3"];
+        [jogador3 setMelhorPont: @15]; [self addJogador:jogador3];
+    */
+    
+    //Ordenação
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"melhorPont" ascending:YES];
+    NSArray *sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
+    NSArray *sortedArray = [jogadores sortedArrayUsingDescriptors:sortDescriptors];
+    
+    for(int i=0; i < n; i++) {
+        Jogador *aux = [sortedArray objectAtIndex: i];
+        [aux ExibirRanking];
+    }
     
     
+    
+    /*
     for(int i=0; i < n; i++) {
     Jogador *aux = [jogadores objectAtIndex: i];
         [aux ExibirRanking];
-    }
+    }*/
     
 }
 
